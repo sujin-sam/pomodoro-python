49062a10bd68c97b6c0ad635de7cc723eb98fd44
Sujin Sam <sujinsam.dodge@gmail.com>
2021-09-18 13:25:29 +0530

    provision to add a custom activity other than work and break

--- cycles.py
+++ cycles.py
@@ -1,19 +1,22 @@
 import time as timeLib
 import activityVoice
 
-def startCycles(cycleCount, workTime, breakTime):
+def startCycles(cycleCount, activityList, activityTimeList):
 
     for cycle in range(cycleCount):
         cycle += 1
         activityVoice.playAudio("Cycle " + str(cycle) + " starts")
 
-        for i in range(2):
-            if i == 0:
-                time = workTime * 60
-                activityVoice.playAudio("Work for Cycle " + str(cycle) + " starts, concentrate harder")
-            else:
-                time = breakTime * 60
-                activityVoice.playAudio("Break for Cycle " + str(cycle) + " starts, Take a break")
+        for i in range(len(activityList)):
+            time = activityTimeList[i] * 60
+            boostWords = ""
+
+            if activityList[i].lower() == "work":
+                boostWords = "concentrate harder"
+            elif activityList[i].lower() == "break":
+                boostWords = "Take a break to fresh up"
+
+            activityVoice.playAudio(activityList[i] + " for Cycle " + str(cycle) + " starts, " + boostWords)
 
             while time:
                 mins, secs = divmod(time, 60)
@@ -22,10 +25,7 @@ def startCycles(cycleCount, workTime, breakTime):
                 timeLib.sleep(1)
                 time -= 1
             
-            if i == 0:
-                activityVoice.playAudio("Work for Cycle " + str(cycle) + " is completed")
-            else:
-                activityVoice.playAudio("Break for Cycle " + str(cycle) + " is completed")
+            activityVoice.playAudio(activityList[i] + " for Cycle " + str(cycle) + " is completed")
         
         activityVoice.playAudio("All the activities of Cycle " + str(cycle) + " are completed")
         
--- pomodoro.py
+++ pomodoro.py
@@ -1,8 +1,17 @@
 import cycles
-            
-# get inputs
-cycleCount = int(input("Enter the number of cycles: "))
-workTime = int(input("Enter the time in minutes for work: "))
-breakTime = int(input("Enter the time in minutes for break: "))
 
-cycles.startCycles(cycleCount, workTime, breakTime)
+cycleCount = 0
+activities = ""
+activityTimeList = []
+
+# get inputs
+cycleCount = int(input("Pls enter the number of cycles: "))
+activities = input("Pls enter the list of activities for each cycle (separated by comma, ex: work,break): ")
+
+activityList = activities.split(',')
+
+for i in range(len(activityList)):
+    timeInput = int(input("Pls enter the time in minutes for " + activityList[i] + ": "))
+    activityTimeList.append(timeInput)
+
+cycles.startCycles(cycleCount, activityList, activityTimeList)
